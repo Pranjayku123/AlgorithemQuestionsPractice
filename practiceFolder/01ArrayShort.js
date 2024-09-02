7adfa55b16949b1d021225f436c295d8f5df7b44
TechHacky <109287745+Pranjayku123@users.noreply.github.com>
2024-09-02 09:22:13 +0000

    new arr

--- practiceFolder/01ArrayShort.js
+++ practiceFolder/01ArrayShort.js
@@ -1,4 +1,21 @@
-const parts = ["shoulders", "knees"];
-const lyrics = ["head", ...parts, "and", "toes"];
-console.log(lyrics); 
- 
+/*Way 01*/
+console.log(lyrics); function spreadArr() {
+    let arr = [2, 12, 6,[21,44,"str"], 0, 'prk', ['pilog', true, 2, 5], false];
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        newArr.push(...arr[i]);
+      } else {
+        newArr.push(arr[i]);
+      }
+    }
+    console.log(newArr);
+  }
+  spreadArr();
+  
+
+//   way02
+  const parts = ["shoulders", "knees"];
+  const lyrics = ["head", ...parts, "and", "toes"];
+  console.log(lyrics); 
+  
